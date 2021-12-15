c427ddbc872f1f3a4eb3fdff7574a9df9e9566e9
Grace Hawkins <amoomajid99@gmail.com>
2021-12-15 03:37:02 +0330

    Connect to network

--- relay.ino
+++ relay.ino
@@ -1,3 +1,5 @@
+#include <ESP8266WiFi.h>
+
 class Relay {
 private:
   int pin;
@@ -44,15 +46,48 @@ class RelayController {
 private:
   Sensor *sensor;
   Relay *relay;
+  WiFiServer *server;
 
 public:
-  RelayController(int sensorPin, int relayPin) {
+  RelayController(int sensorPin, int relayPin, int port) {
     sensor = new Sensor(sensorPin);
     relay = new Relay(relayPin);
+
+    server = new WiFiServer(port);
+    server->begin();
+    server->setNoDelay(true);
+  }
+
+  ~RelayController() {
+    server->close();
+    delete server;
+    delete sensor;
+    delete relay;
   }
 
   void stepForward() {
     relay->setState(sensor->getState() ^ relay->getState());
+
+    WiFiClient client = server->available();
+    if (!client)
+      return;
+    while (client.connected()) {
+      if (client.available()) {
+        String line = client.readStringUntil('\n');
+        if (line[1] == '0') {
+          relay->setState(0);
+          client.println(getState());
+        }
+        else if (line[1] == '1') {
+          relay->setState(1);
+          client.println(getState());
+        }
+        else if (line[1] == '?') {
+          client.println(getState());
+        }
+      }
+    }
+    client.stop();
   }
 
   bool getState() {
@@ -63,7 +98,15 @@ public:
 RelayController *controller;
 
 void setup() {
-  controller = new RelayController(16, 5);
+  Serial.begin(9600);
+  WiFi.config(IPAddress(10,42,0,200), IPAddress(10,42,0,1), IPAddress(255,255,255,0));
+  WiFi.begin("SSID", "PASSWORD");
+  WiFi.setAutoReconnect(true);
+  if(WiFi.waitForConnectResult() == WL_CONNECTED)
+    Serial.println("Connected");
+  else
+    Serial.println("Failed");
+  controller = new RelayController(16, 5, 8000);
 }
 
 void loop() {
