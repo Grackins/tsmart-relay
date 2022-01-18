cc9419a32f17c02c9f006f1ae6fb7ed4d5b3f94a
Grace Hawkins <amoomajid99@gmail.com>
2022-01-18 20:20:38 +0330

    Add comments to be more readable

--- relay.ino
+++ relay.ino
@@ -1,8 +1,15 @@
 #include <ESP8266WiFi.h>
 #include <EEPROM.h>
 
+// A magic number to store in first 2 bytes of EEPROM to check its validity
 #define MAGIC 0b10100101
 
+
+/*
+ * Relay class receives a pin number in its constructor and manages
+ * a relay connected to that pin. One can call setState to set the
+ * state of the relay or call getState to just read it
+ */
 class Relay {
 private:
   int pin;
@@ -25,6 +32,13 @@ public:
   }
 };
 
+
+/*
+ * Sensor class gets a pin number in its constructor and creates an
+ * interface to read data from touch sensor.
+ * getState function return true in a cycle that state of sensor
+ * changes from LOW to HIGH.
+ */
 class Sensor {
 private:
   int pin;
@@ -45,6 +59,24 @@ public:
   }
 };
 
+
+/*
+ * Relay Controller class combines Sensor and Relay class
+ * It's used to connect these two elements and change the state
+ * of the relay based on sensor's state.
+ * Also it listena to a TCP socket and waits for commands from
+ * the master.
+ * The available commands are:
+ *    000 -> sets the relay state to open
+ *    111 -> sets the relay state to closed
+ *    ??? -> asks about current state of the relay
+ * In response to each command, the server will respond with
+ * the new state of relay.
+ * 
+ * In each of the main program's iterations the stepForward
+ * function should be called in order to process new commands
+ * and sensor events.
+ */
 class RelayController {
 private:
   Sensor *sensor;
@@ -98,8 +130,12 @@ public:
   }
 };
 
+// Create a list of relay controllers
+// There are 2 relays so the length of the array is 2
 RelayController *controller[2] = {NULL, NULL};
 
+
+// Receive a string and cast it to IPAddress
 IPAddress str2ip(String ip, int *errno) {
   int pos0 = ip.indexOf('.');
   int pos1 = ip.indexOf('.', pos0 + 1);
@@ -117,6 +153,8 @@ IPAddress str2ip(String ip, int *errno) {
    );
 }
 
+// Store a network config on EEPROM
+// The config contains ssid, password, myIp, netmask and default gateway's IP address
 void saveNetConfig(String ssid, String passwd, IPAddress myIp, IPAddress netmask, IPAddress gateway) {
   int pos = 0;
   EEPROM.begin(64);
@@ -133,12 +171,18 @@ void saveNetConfig(String ssid, String passwd, IPAddress myIp, IPAddress netmask
   EEPROM.end();
 }
 
+// Load network config from EEPROM
 void loadNetConfig() {
   int pos = 0;
   byte value = 0;
 
   bool valid = true;
   EEPROM.begin(64);
+
+  /*
+   * If we have written data on EEPROM, the first 2 bytes
+   * must be equal to MAGIC
+   */
   value = EEPROM.read(pos++);
   if (value != MAGIC) valid = false;
   value = EEPROM.read(pos++);
@@ -161,6 +205,12 @@ void loadNetConfig() {
   WiFi.waitForConnectResult();
 }
 
+/*
+ * A handler function to handle network config command
+ * in serial port
+ * This is all about parsing the string and passing the
+ * data to saveNetConfig function
+ */
 void networkHandler(String cmd) {
   if (cmd[0] == '?') {
     Serial.print("Local IP:\t");
@@ -215,6 +265,10 @@ void networkHandler(String cmd) {
   Serial.println("Saved new config. Reboot to make effect.");
 }
 
+/*
+ * A function to manage serial shell
+ * It should be called in each iteration of main loop
+ */
 void processShell() {
   if (!Serial.available())
     return;
@@ -228,8 +282,14 @@ void processShell() {
 }
 
 void setup() {
+  // Start serial port, it's used for configuration commands
   Serial.begin(9600);
+
+  // Load last network config from EEPROM
   loadNetConfig();
+
+  // Initialize relay controllers
+  // 16, 5, 13, 12 are the pins connected to sensors and relays
   controller[0] = new RelayController(16, 5, 8000);
   controller[1] = new RelayController(13, 12, 8001);
 }
